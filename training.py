f1504aca26e7267ebe33686910a24ba4c7366e94
richard-guinto <32997759+richard-guinto@users.noreply.github.com>
2017-10-22 20:22:33 +0800

    Initial commit of training model

---
+++ training.py
@@ -0,0 +1,150 @@
+'''
+Author: Richard Guinto
+Project: DeepHomography
+Dependencies: keras
+Usage: python <this file>
+'''
+
+
+# From baudm
+# Efficient loading in Keras using a Python generator
+
+import os.path
+import glob
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Dropout, Conv2D, MaxPooling2D, Flatten, BatchNormalization, InputLayer
+from keras.callbacks import ModelCheckpoint, ProgbarLogger
+from keras import backend as K
+from keras import optimizers
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+
+def data_loader(path, batch_size=64):
+    """Generator to be used with model.fit_generator()"""
+    while True:
+        for npz in glob.glob(os.path.join(path, '*.npz')):
+            # Load pack into memory
+            archive = np.load(npz)
+            images = archive['images']
+            offsets = archive['offsets']
+            # Yield minibatch
+            for i in range(0, len(offsets), batch_size):
+                end_i = i + batch_size
+                try:
+                    batch_images = images[i:end_i]
+                    batch_offsets = offsets[i:end_i]
+                except IndexError:
+                    continue
+                # Normalize
+                batch_images = (batch_images - 127.5) / 127.5
+                batch_offsets = batch_offsets / 32.
+                yield batch_images, batch_offsets
+#end of baudm code
+
+def euclidean_l2(y_true, y_pred):
+    return K.sqrt(K.sum(K.square(y_pred - y_true), axis=-1, keepdims=True))
+#    return K.sqrt(K.maximum(K.sum(K.square(y_pred - y_true), axis=-1, keepdims=True), K.epsilon()))
+
+# Dataset-specific
+#train_data_path = '/Users/richard/MSEE/deephomography/dataset/training'
+#test_data_path = '/Users/richard/MSEE/deephomography/dataset/test'
+train_data_path = '/home/samsung/richard/dataset/training'
+test_data_path = '/home/samsung/richard/dataset/test'
+#num_samples = 50 * 9216 # 53 archives x 9,216 samples per archive, but use just 50 and save the 3 for testing
+num_samples = 40 * 9216 # 43 archives x 9,216 samples per archive, but use just 40 and save the 3 for testing
+
+# From the paper
+batch_size = 64
+total_iterations = 90000
+
+steps_per_epoch = num_samples / batch_size # As stated in Keras docs
+epochs = int(total_iterations / steps_per_epoch)
+#steps_per_epoch = 10
+#epochs = 10
+
+input_shape = (128, 128, 2)
+kernel_size = 3
+pool_size = 2
+filters = 64
+dropout = 0.5
+
+
+model = Sequential()
+model.add(InputLayer(input_shape))
+model.add(Conv2D(filters=filters,\
+        kernel_size=kernel_size, activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=filters,\
+        kernel_size=kernel_size, activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size))
+model.add(Conv2D(filters=filters,\
+        kernel_size=kernel_size, activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=filters,\
+        kernel_size=kernel_size, activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size))
+model.add(Conv2D(filters=filters*2,\
+        kernel_size=kernel_size, activation='relu', padding='same',))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=filters*2,\
+        kernel_size=kernel_size, activation='relu', padding='same',))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size))
+model.add(Conv2D(filters=filters*2,\
+        kernel_size=kernel_size, activation='relu', padding='same',))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=filters*2,\
+        kernel_size=kernel_size, activation='relu', padding='same',))
+model.add(BatchNormalization())
+model.add(Flatten())
+model.add(Dropout(dropout))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(dropout))
+#for regression model
+model.add(Dense(8))
+#model.add(Activation('softmax'))
+model.summary()
+
+# load weights
+#model.load_weights("/data/deephomography-00-1.65.hdf5")
+
+#ues optimizer Stochastic Gradient Methond with a Learning Rate of 0.005 and momentus of 0.9
+#sgd = optimizers.SGD(lr=0.005, momentum=0.9, decay=0.001355)
+sgd = optimizers.SGD(lr=0.005, momentum=0.9)
+
+#compile model
+model.compile(loss=euclidean_l2,\
+        optimizer=sgd, metrics=['mean_squared_error'])
+
+#check point
+filepath = "/data/richard/20171022/weights-{epoch:02d}-{loss:.2f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1)
+progbar = ProgbarLogger('steps')
+#learning rate scheduler
+
+
+#callback_list = [progbar, checkpoint]
+callback_list = [checkpoint]
+
+
+# share GPU
+#config = tf.ConfigProto()
+#config.gpu_options.per_process_gpu_memory_fraction = 0.5
+#set_session(tf.Session(config=config))
+
+# Train
+print('TRAINING...')
+model.fit_generator(data_loader(train_data_path, batch_size),
+                    steps_per_epoch=steps_per_epoch,
+                    epochs=epochs, callbacks=callback_list)
+
+print('TESTING...')
+# Test
+score = model.evaluate_generator(data_loader(test_data_path, batch_size),
+                         steps=3*9216/batch_size)
+
+print('score: ', score)
