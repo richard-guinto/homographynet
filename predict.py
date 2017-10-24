212a5fc0cca5808f748d1e54bdd38822d0cf19d5
richard-guinto <32997759+richard-guinto@users.noreply.github.com>
2017-10-24 09:20:46 +0800

    Updated python scripts

--- predict.py
+++ predict.py
@@ -7,7 +7,7 @@ Usage: python <this file>
 
 
 import os.path
-import glob
+#import glob
 
 import numpy as np
 from keras.models import load_model
@@ -21,8 +21,8 @@ def euclidean_l2(y_true, y_pred):
 def mean_square_error(y_true, y_pred):
     return K.mean(K.square(y_pred - y_true), axis=-1, keepdims=True)
 
-# load model
-model = load_model("/data/richard/model.hdf5", custom_objects={'euclidean_l2': euclidean_l2})
+#checkpoint
+checkpoint = "/data/richard/model.hdf5"
 
 # Dataset-specific
 predict_data_path = '/home/samsung/richard/dataset/test/f7823b11-901a-457a-89a2-8c37dc4fef60.npz'
@@ -30,51 +30,50 @@ samples_per_archive = 9216
 
 
 
+# load model
+print('Loading model... ', checkpoint)
+model = load_model(checkpoint, custom_objects={'euclidean_l2': euclidean_l2})
+
+print('Loading archive... ', os.path.abspath(predict_data_path))
 archive = np.load(os.path.abspath(predict_data_path))
 print('keys: ', archive.files)
 images = archive['images']
 offsets = archive['offsets']
-sample_image = images[0];
-sample_shape = sample_image.shape
-print('sample shape: ', sample_shape)
-sample_image = sample_image.reshape(1, 128, 128, 2)
-#print('sample image shape: ', sample_image.shape)
-sample_offset = offsets[0];
-print('Sample Offset: ', sample_offset)
-normalize_image = (sample_image - 127.5) / 127.5
-normalize_offset = sample_offset / 32.
 
+for idx in range(10):
+    print('*************** SAMPLE ', idx)
+    sample_image = images[idx];
+    sample_shape = sample_image.shape
+    sample_image = sample_image.reshape(1, 128, 128, 2)
+    sample_offset = offsets[idx];
+    print('Sample Offset: ', sample_offset)
+    norm_sample_image = (sample_image - 127.5) / 127.5
+    norm_sample_offset = sample_offset / 32.
+    print('Normalize Sample Offset: ', norm_sample_offset)
 
-print('Normalize Sample Offset: ', normalize_offset)
-offset = model.predict(normalize_image)
-print('Predicted Nomralize Offset: ', offset)
-pred_nonnorm_offset = offset * 32.
-print('Predicted Non-normalize Offset: ', pred_nonnorm_offset)
-smse = np.sqrt(np.sum(np.square(offset - normalize_offset),axis=-1,keepdims=True))
-print('Normalize SMSE: ', smse)
-mse = np.mean(np.square(offset - normalize_offset), axis=-1, keepdims=True)
-print('Normalize MSE: ', mse)
-nonnorm_mse = np.mean(np.square(pred_nonnorm_offset - sample_offset), axis=-1, keepdims=True)
-print('Non-Normalize MSE: ', nonnorm_mse)
-mae = np.mean(np.absolute(offset - normalize_offset), axis=-1, keepdims=True)
-print('Normalize MAE: ', mae)
-nonnorm_mae = np.mean(np.absolute(pred_nonnorm_offset - sample_offset), axis=-1, keepdims=True)
-print('Non-Normalize MAE: ', nonnorm_mae)
-sum = 0
-for i in range(0, len(sample_offset),2):
-    h = np.square(pred_nonnorm_offset[0][i] - sample_offset[i]) + np.square(pred_nonnorm_offset[0][i+1] - sample_offset[i+1])
-    h = np.sqrt(h)
-    print('h: ', h)
-    sum = sum + h
-sum = sum / (len(sample_offset) / 2)
-print('average corner error: ', sum)
+    print('Predicting Offset...')
+    norm_pred_offset = model.predict(norm_sample_image)
+    print('Predicted Offset(Normalize): ', norm_pred_offset)
+    pred_offset = norm_pred_offset * 32.
+    print('Predicted Offset: ', pred_offset)
+    norm_rmse = np.sqrt(np.sum(np.square(norm_pred_offset - norm_sample_offset),axis=-1,keepdims=True))
+    print('Normalize RMSE: ', norm_rmse)
+    norm_mse = np.mean(np.square(norm_pred_offset - norm_sample_offset), axis=-1, keepdims=True)
+    print('Normalize MSE: ', norm_mse)
+    mse = np.mean(np.square(pred_offset - sample_offset), axis=-1, keepdims=True)
+    print('MSE: ', mse)
+    norm_mae = np.mean(np.absolute(norm_pred_offset - norm_sample_offset), axis=-1, keepdims=True)
+    print('Normalize MAE: ', norm_mae)
+    mae = np.mean(np.absolute(pred_offset - sample_offset), axis=-1, keepdims=True)
+    print('MAE: ', mae)
+    sum = 0
+    for i in range(0, len(sample_offset),2):
+        h = np.square(pred_offset[0][i] - sample_offset[i]) + np.square(pred_offset[0][i+1] - sample_offset[i+1])
+        h = np.sqrt(h)
+        print('h: ', h)
+        sum = sum + h
+    sum = sum / (len(sample_offset) / 2)
+    print('Ave. Corner Error: ', sum)
 
-offset = model.predict(sample_image)
-print('Predicted Offset: ', offset)
-print('Target Offset: ', sample_offset)
-smse = np.sqrt(np.sum(np.square(offset - sample_offset),axis=-1,keepdims=True))
-print('SMSE: ', smse)
-mse = np.mean(np.square(offset - sample_offset), axis=-1, keepdims=True)
-print('MSE: ', mse)
 
 
--- testing.py
+++ testing.py
@@ -14,7 +14,7 @@ import glob
 
 import numpy as np
 from keras.models import load_model
-#from keras.layers import Activation, Dense, Dropout, Conv2D, MaxPooling2D, Flatten, BatchNormalization, InputLayer
+from keras.layers import Activation, Dense, Dropout, Conv2D, MaxPooling2D, Flatten, BatchNormalization, InputLayer
 from keras import backend as K
 from keras import optimizers
 
@@ -44,24 +44,29 @@ def euclidean_l2(y_true, y_pred):
     return K.sqrt(K.sum(K.square(y_pred - y_true), axis=-1, keepdims=True))
 #    return K.sqrt(K.maximum(K.sum(K.square(y_pred - y_true), axis=-1, keepdims=True), K.epsilon()))
 
+#modify this path if your system
+checkpoint = "/data/richard/model.hdf5"
+
+# load model
+model = load_model(checkpoint, custom_objects={'euclidean_l2': euclidean_l2})
+
 # Dataset-specific
 train_data_path = '/home/samsung/richard/dataset/training'
 test_data_path = '/home/samsung/richard/dataset/test'
-num_samples = 3 * 9216 # 43 archives x 9,216 samples per archive, but use just 40 and save the 3 for testing
+samples_per_archive = 9216
+num_archive = 3
+num_samples = num_archive * samples_per_archive # 3 archives (out of 43) x 9,216 samples per archive for testing
 
 # From the paper
 batch_size = 64
 total_iterations = 90000
 
-steps_per_epoch = num_samples / batch_size # As stated in Keras docs
+steps = num_samples / batch_size # As stated in Keras docs
 epochs = int(total_iterations / steps_per_epoch)
 
 
-# load model
-model = load_model("/data/richard/model.hdf5")
-
 #ues optimizer Stochastic Gradient Methond with a Learning Rate of 0.005 and momentus of 0.9
-sgd = optimizers.SGD(lr=0.005, momentum=0.9, decay=0.001355)
+sgd = optimizers.SGD(lr=0.005, momentum=0.9)
 
 #compile model
 model.compile(loss=euclidean_l2,\
@@ -70,7 +75,7 @@ model.compile(loss=euclidean_l2,\
 
 # Test
 score = model.evaluate_generator(data_loader(test_data_path, batch_size),
-                         steps=steps_per_epoch)
+                         steps=steps)
 
 print('Test score:', score)
 
